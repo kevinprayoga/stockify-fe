a0deca14bb596fad359eb84bbe0a3cb68b1e47dc
attariqazhar <18221043@mahasiswa.itb.ac.id>
2024-05-15 19:10:14 +0700

    deleted unused library

--- src/Tab/Cart.jsx
+++ src/Tab/Cart.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { ScrollView, Text, TextInput, Touchable, TouchableOpacity, View } from "react-native";
-// import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
-import { MaterialIcons } from '@expo/vector-icons';
+import { ScrollView, Text, TextInput, TouchableOpacity, View } from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { Octicons } from '@expo/vector-icons';
 import { useFonts } from 'expo-font';
