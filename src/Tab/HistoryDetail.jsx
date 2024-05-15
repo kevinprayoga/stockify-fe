aee2783f9d3072369e218b6f70dce6fd8b5dae8f
attariqazhar <18221043@mahasiswa.itb.ac.id>
2024-05-15 16:58:36 +0700

    added total section on detailed history card

--- src/Tab/HistoryDetail.jsx
+++ src/Tab/HistoryDetail.jsx
@@ -1,21 +1,10 @@
 import React, {useState} from "react";
 import { ScrollView, Text, TextInput, Touchable, TouchableOpacity, View } from "react-native";
-// import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
 import { MaterialIcons } from '@expo/vector-icons';
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { Octicons } from '@expo/vector-icons';
 
 export default function HistoryDetail() {
-  const [isSelesaiActive, setIsSelesaiActive] = useState(true);
-
-  const handleBatalPress = () => {
-    setIsSelesaiActive(false);
-  };
-
-  const handleSelesaiPress = () => {
-    setIsSelesaiActive(true);
-  };
-
   return (
     <View className="bg-[#F5F6F7]">
       <ScrollView className="mt-[30] h-screen bg-[#F5F6F7]">
@@ -28,9 +17,9 @@ export default function HistoryDetail() {
             </TouchableOpacity>    
         </View>
 
-        <View className="mx-[27] mt-[20] bg-white rounded-xl p-[15]">
-            <View className="border-b border-gray-200 flex-row justify-between items-center ">
-                <View className="pb-[8]">
+        <View className="mx-[27] mt-[20] bg-white rounded-xl">
+            <View className="border-b border-gray-200 flex-row justify-between items-center p-[15]">
+                <View>
                     <Text className="text-xl font-bold">#0376</Text>
                     <Text className="text-[13px] text-gray-400">20/03/2024</Text>
                 </View>
@@ -40,7 +29,7 @@ export default function HistoryDetail() {
                 </View>
             </View>
             {/* Daftar Item */}
-            <View>
+            <View className="px-[15] py-[5]">
                 <View className="flex-row justify-between items-center">
                     <View className="flex-row p-[10]">
                         <Text className="w-[30]">1</Text><Text className="">Mie Goreng</Text>
@@ -51,66 +40,16 @@ export default function HistoryDetail() {
                     <View className="flex-row p-[10]">
                         <Text className="w-[30]">10</Text><Text className="">Koko Krunch</Text>
                     </View>
-                    <Text>Rp125.000</Text>
+                    <Text>Rp200.000</Text>
                 </View>
             </View>
             {/* Total */}
-            <View>
-                
+            <View className="bg-[#5A4DF3] rounded-b-lg px-[15] py-[10] flex-row justify-between">
+                <Text className="text-lg text-white font-bold">Total</Text>
+                <Text className="text-lg text-white font-bold">Rp325.000</Text>
             </View>
         </View>
 
-
-
-        <View>
-            {/* Box Selesai */}
-            <View className="mx-[27] bg-white mt-[15] rounded-lg p-[15] shadow">
-                <View className="flex-row justify-between items-center border-b border-gray-200 pb-[8]">
-                <Text className="text-lg font-bold">#0376</Text>
-                <Text className="text-[13px] text-gray-400">20/03/2024</Text>
-                </View>
-                <View className="flex-row justify-between items-center border-bottom pt-[8]">
-                <View className="flex-row">
-                    <Text className="text-lg font-bold text-gray-500">Item: </Text><Text className="text-lg font-bold">5</Text>
-                </View>
-                <View className="flex-row">
-                    <Text className="text-lg font-bold text-gray-500">Total: </Text><Text className="text-lg font-bold">Rp200.000</Text>
-                </View>
-                </View>
-                <View className="flex-row justify-between items-center border-bottom pt-[25]">
-                <View className="flex-row">
-                    <Text className="text-[#27AE60] font-bold">Pembayaran Sukses</Text>
-                </View>
-                <TouchableOpacity className="bg-[#5A4DF3] w-[80] py-[5] rounded-lg items-center">
-                    <Text className="text-white text-lg font-bold">Detail</Text>
-                </TouchableOpacity>
-                </View>
-            </View>
-
-            {/* Box Batal */}
-            <View className="mx-[27] bg-white mt-[15] rounded-lg p-[15] shadow">
-                <View className="flex-row justify-between items-center border-b border-gray-200 pb-[8]">
-                <Text className="text-lg font-bold">#0376</Text>
-                <Text className="text-[13px] text-gray-400">20/03/2024</Text>
-                </View>
-                <View className="flex-row justify-between items-center border-bottom pt-[8]">
-                <View className="flex-row">
-                    <Text className="text-lg font-bold text-gray-500">Item: </Text><Text className="text-lg font-bold">5</Text>
-                </View>
-                <View className="flex-row">
-                    <Text className="text-lg font-bold text-gray-500">Total: </Text><Text className="text-lg font-bold">Rp200.000</Text>
-                </View>
-                </View>
-                <View className="flex-row justify-between items-center border-bottom pt-[25]">
-                <View className="flex-row">
-                    <Text className="text-[#F13131] font-bold">Pembayaran Batal</Text>
-                </View>
-                <TouchableOpacity className="bg-[#5A4DF3] w-[80] py-[5] rounded-lg items-center">
-                    <Text className="text-white text-lg font-bold">Detail</Text>
-                </TouchableOpacity>
-                </View>
-            </View>
-        </View>
       </ScrollView>
     </View>
   );
